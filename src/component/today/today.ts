efded335d24a0f02542d3640fbd036b98172f77c
daikidaiki1583 <naka.dai.270120@gmail.com>
2020-12-08 21:24:05 +0900

    日付計算修正

--- src/component/today/today.ts
+++ src/component/today/today.ts
@@ -1,8 +1,8 @@
 export const calculateToday = () => {
     const date = new Date();    
     const year = date.getFullYear();
-    const month = date.getMonth() + 1;
-    const day = date.getDate();
+    const month = ('0'+(date.getMonth()+1)).slice(-2);
+    const day = ('0'+date.getDate()).slice(-2);
     console.log(year+'-'+month+'-'+day)
     return year+'-'+month+'-'+day
 }
